8eb962a83914310e046a18d5a015d9872510b6f7
Keith Armstrong <keith.w.armstrong@gmail.com>
2025-06-04 21:54:12 -0500

    Update main.jsx
    updated import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AutoRefinanceHome from './App';
+import AutoRefinanceHome from './AutoRefinanceHome';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AutoRefinanceHome />
   </React.StrictMode>
 );
+
+
